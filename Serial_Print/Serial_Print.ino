1cca222ac01e4a276bd8f1167323fdeb98b36f85
Fazal Hameed <153935671+fazalhameed@users.noreply.github.com>
2024-07-10 13:00:30 +0500

    Add files via upload

---
+++ Serial_Print/Serial_Print.ino
@@ -0,0 +1,13 @@
+
+void setup()
+{
+  Serial.begin(9600);
+}
+ 
+void loop()
+{
+  Serial.println("Hello");
+   Serial.println("Fazal Hameed Here");
+    Serial.println("Welcome To Chip Soul");
+  delay(1000);
+}
